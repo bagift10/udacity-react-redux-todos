b14474c2bef20e1754471f61098c7f861168fa66
Brandon Gift <bagift10@gmail.com>
2021-06-11 21:34:53 -0400

    feat: Create subscribe function to listen for state changes

--- index.js
+++ index.js
@@ -5,9 +5,29 @@ function createStore () {
   // 3. Listen for changes to the state
   // 4. Update the state
 
+  // 1. Create the state
   let state;
+  let listeners = [];
 
+  // 2. Get the state
   const getState = () => state;
 
-  return { getState };
+  // 3. Listen for changes to the state
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  }
+
+  return {
+    getState,
+    subscribe
+  };
 }
+
+const store = createStore();
+store.subscribe(() => {
+  console.log('The new state is: ', store.state);
+});
+
+store.subscribe(() => {
+  console.log('The state changed.');
+});
